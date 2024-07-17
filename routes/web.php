8ff72275fcc74d700f3602f0ccd7a9697c4600c0
akmmp241 <akm.mp24@gmail.com>
2024-07-17 14:31:46 +0700

    improve routes

--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,7 @@ Route::get('/user/manage', function () {
 
 Route::get('/dashboard', fn() => view('dashboard'));
 
-Route::prefix('/payment-link')->group(function () {
+Route::prefix('/payment-links')->group(function () {
     Route::get('/', fn() => view('payment-links'));
 
     Route::get('/create', fn() => view('create-payment-link'));
@@ -29,7 +29,12 @@ Route::prefix('/payment-link')->group(function () {
 
 Route::prefix('/settings')->group(function () {
     Route::get('/', fn() => view('settings.settings'));
-    Route::get('/set-xendit-api-key', fn() => view('settings.set-xendit-api-key'));
-    Route::get('/set-default-payment-methods', fn() => view('settings.set-default-payment-methods'));
-    Route::get('/webhook', fn() => view('settings.set-webhook'));
+
+    Route::prefix('/xendit')->group(function () {
+        Route::get('/api-key', fn() => view('settings.set-xendit-api-key'));
+        Route::get('/webhook', fn() => view('settings.set-webhook'));
+    });
+
+    Route::get('/payment-methods', fn() => view('settings.set-default-payment-methods'));
+
 });
