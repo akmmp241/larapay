2b22c4e94ca15a76c852b97deaa4f6e2e79a4b8c
akmmp241 <akmalmp241@gmail.com>
2024-08-06 11:36:35 +0700

    Update navbar

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -20,27 +20,20 @@
                 <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">
                     <img src="{{asset("dist/assets/img/avatar.png")}}" class="user-image rounded-circle shadow"
                          alt="User Image">
-                    <span class="d-none d-md-inline">Akmal Muhammad Pridianto</span>
+                    <span class="d-none d-md-inline">{{ auth()->user()->first_name . " " . auth()->user()->last_name }}</span>
                 </a>
                 <ul class="dropdown-menu dropdown-menu-lg dropdown-menu-end"> <!--begin::User Image-->
                     <li class="user-header text-bg-primary">
-                        <img src="{{asset("dist")}}/assets/img/user2-160x160.jpg" class="rounded-circle shadow"
+                        <img src="{{asset("dist/assets/img/avatar.png")}}" class="rounded-circle shadow"
                              alt="User Image">
                         <p>
-                            Alexander Pierce - Web Developer
-                            <small>Member since Nov. 2023</small>
+                            {{ auth()->user()->first_name . " " . auth()->user()->last_name }}
+                            <small>Member since {{ auth()->user()->created_at->format("M, Y") }}</small>
                         </p>
                     </li> <!--end::User Image--> <!--begin::Menu Body-->
-                    <li class="user-body"> <!--begin::Row-->
-                        <div class="row">
-                            <div class="col-4 text-center"><a href="#">Followers</a></div>
-                            <div class="col-4 text-center"><a href="#">Sales</a></div>
-                            <div class="col-4 text-center"><a href="#">Friends</a></div>
-                        </div> <!--end::Row-->
-                    </li> <!--end::Menu Body--> <!--begin::Menu Footer-->
                     <li class="user-footer">
-                        <a href="#" class="btn btn-default btn-flat">Profile</a>
-                        <a href="#" class="btn btn-default btn-flat float-end">Sign out</a></li> <!--end::Menu Footer-->
+                        <a href="{{ route('profile') }}" class="btn btn-default btn-flat">Profile</a>
+                        <a href="#" class="btn btn-default btn-flat float-end">Log out</a></li> <!--end::Menu Footer-->
                 </ul>
             </li> <!--end::User Menu Dropdown-->
         </ul> <!--end::End Navbar Links-->
